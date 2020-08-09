3ea3187b42d12b7f2d6dd48ab01221b351f6d0cc
Tushar Raj <tushar.raj@outlook.com>
2020-08-09 15:13:36 +0530

    form scaffolding

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 const material = [
     MatToolbarModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
 ];
 
 @NgModule({
