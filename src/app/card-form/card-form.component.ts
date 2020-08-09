8a413ded9609c924aa9154c924751af39da3b0b4
Tushar Raj <tushar.raj@outlook.com>
2020-08-09 15:39:25 +0530

    form layout

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-card-form',
-  templateUrl: './card-form.component.html',
-  styleUrls: ['./card-form.component.scss']
+    selector: 'app-card-form',
+    templateUrl: './card-form.component.html',
+    styleUrls: ['./card-form.component.scss']
 })
 export class CardFormComponent implements OnInit {
 
-  constructor() { }
+    months: Array<number>;
+    years: Array<number>;
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+    ngOnInit(): void {
+        this.months = Array.from(Array(12), (_, i) => i+1);
+        this.years = Array.from(Array(20), (_, i) => i+2020);
+    }
 
 }
